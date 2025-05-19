c1ca4f31260dbbbdf1933499bb1b6a3f767e7b81
Marlon cano <marloncano1997@gmail.com>
2025-05-18 20:47:34 -0500

    Inicio

---
+++ README.md
@@ -0,0 +1,4 @@
+# cinemapedia
+
+1. Copiar el .env.template y renombrarlo a .env
+
---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  ThemeData getThema() =>
+      ThemeData(useMaterial3: true, colorSchemeSeed: Colors.amberAccent);
+}
